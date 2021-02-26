70e07fee2d2debf9e55065396035af089a1f2093
Gnatchecko Boris <sirob-ig@users.noreply.github.com>
2021-02-26 22:24:04 +0000

    format document

--- get-result.js
+++ get-result.js
@@ -1,53 +1,54 @@
 module.exports = function krestikiNoliki(arr) {
-switch('xxx'){
-  case arr[0].join(''):
-    return 'вы выйграли';
-    break;
-  case arr[1].join(''):
-    return 'вы выйграли';
-    break;
-  case arr[2].join(''):
-    return 'вы выйграли';
-    break;
-  case [arr[0][0],arr[1][0],arr[2][0]].join(''):
-    return 'вы выйграли';
-    break;
-  case [arr[0][1],arr[1][1],arr[2][1]].join(''):
-    return 'вы выйграли';
-    break;
-  case [arr[0][2],arr[1][2],arr[2][2]].join(''):
-    return 'вы выйграли';
-    break;
-  case [arr[0][0],arr[1][1],arr[2][2]].join(''):
-    return 'вы выйграли';
-    break;
-  case [arr[2][0],arr[1][1],arr[0][2]].join(''):
-    return 'вы выйграли';
-    break;
-}
-switch('ooo') {
-  case arr[0].join(''):
-    return 'вы проиграли';
-    break;
-  case arr[1].join(''):
-    return 'вы проиграли';
-    break;
-  case arr[2].join(''):
-    return 'вы проиграли';
-    break;
-  case [arr[0][0],arr[1][0],arr[2][0]].join(''):
-    return 'вы проиграли';
-    break;
-  case [arr[0][1],arr[1][1],arr[2][1]].join(''):
-    return 'вы проиграли';
-    break;
-  case [arr[0][2],arr[1][2],arr[2][2]].join(''):
-    return 'вы проиграли';
-    break;
-  case [arr[0][0],arr[1][1],arr[2][2]].join(''):
-    return 'вы проиграли';
-    break;
-  case [arr[2][0],arr[1][1],arr[0][2]].join(''):
-    return 'вы проиграли';
-    break;}
+    switch('xxx'){
+      case arr[0].join(''):
+        return 'вы выйграли';
+        break;
+      case arr[1].join(''):
+        return 'вы выйграли';
+        break;
+      case arr[2].join(''):
+        return 'вы выйграли';
+        break;
+      case [arr[0][0],arr[1][0],arr[2][0]].join(''):
+        return 'вы выйграли';
+        break;
+      case [arr[0][1],arr[1][1],arr[2][1]].join(''):
+        return 'вы выйграли';
+        break;
+      case [arr[0][2],arr[1][2],arr[2][2]].join(''):
+        return 'вы выйграли';
+        break;
+      case [arr[0][0],arr[1][1],arr[2][2]].join(''):
+        return 'вы выйграли';
+        break;
+      case [arr[2][0],arr[1][1],arr[0][2]].join(''):
+        return 'вы выйграли';
+        break;
+    }
+    switch('ooo') {
+      case arr[0].join(''):
+        return 'вы проиграли';
+        break;
+      case arr[1].join(''):
+        return 'вы проиграли';
+        break;
+      case arr[2].join(''):
+        return 'вы проиграли';
+        break;
+      case [arr[0][0],arr[1][0],arr[2][0]].join(''):
+        return 'вы проиграли';
+        break;
+      case [arr[0][1],arr[1][1],arr[2][1]].join(''):
+        return 'вы проиграли';
+        break;
+      case [arr[0][2],arr[1][2],arr[2][2]].join(''):
+        return 'вы проиграли';
+        break;
+      case [arr[0][0],arr[1][1],arr[2][2]].join(''):
+        return 'вы проиграли';
+        break;
+      case [arr[2][0],arr[1][1],arr[0][2]].join(''):
+        return 'вы проиграли';
+        break;
+      }
 }
